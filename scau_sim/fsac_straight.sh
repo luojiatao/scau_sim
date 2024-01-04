0a11696061517f0d28a31e36a6c0c00a4c593720
YChengg <a165383@qq.com>
2024-01-04 16:16:20 +0800

    小更新
    更新.gitignore和直线的sh启动文件，不用fssim的车模

--- scau_sim/fsac_straight.sh
+++ scau_sim/fsac_straight.sh
@@ -2,7 +2,7 @@
 
 gnome-terminal --tab -- bash -c "\
 source devel/setup.bash; \
-roslaunch gazebo_nav total_straight_fssim.launch; \
+roslaunch gazebo_nav total_straight.launch; \
 exec bash"
 
 # 两个roslauch之间需要间隔一段时间，否则会相互抢占roscore
